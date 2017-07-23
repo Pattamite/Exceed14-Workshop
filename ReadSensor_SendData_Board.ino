4a80595c228bb222f5794d60bd6d0b3fb7cccba1
Pattnatt <jittapat007@gmail.com>
2017-07-23 17:30:26 +0700

    Upload file

---
+++ ReadSensor_SendData_Board.ino
@@ -0,0 +1,99 @@
+#include <UnoWiFiDevEd.h>
+#include <Wire.h>
+#include <Servo.h>
+#include <dht.h>
+
+#define CONNECTOR         "rest" 
+#define SERVER_ADDR       "158.108.165.223"
+
+#define ULTRA_1_TRIG_PIN  13
+#define ULTRA_1_ECHO_PIN  12
+#define ULTRA_2_TRIG_PIN  11
+#define ULTRA_2_ECHO_PIN  10
+#define DHT11_PIN         2
+#define LDR_PIN           A0
+
+#define DELAY_TIME        1000
+
+dht DHT;
+
+long ultra_duration;
+long ultra_distance_1;
+long ultra_distance_2;
+int light;
+int temperature;
+
+String sendStr;
+
+String sendLdr = "/data/workshop/6/set/";
+String sendTemp = "/data/workshop/4/set/";
+String sendUltra = "/data/workshop/10/set/";
+
+CiaoData data;
+
+void taskValue(){
+  //Outdoor Ultrasonic
+  digitalWrite(ULTRA_1_TRIG_PIN, LOW);
+  delay(2);
+  digitalWrite(ULTRA_1_TRIG_PIN, HIGH);
+  delay(10);
+  digitalWrite(ULTRA_1_TRIG_PIN, LOW);
+  ultra_duration = pulseIn(ULTRA_1_ECHO_PIN, HIGH);
+  ultra_distance_1 = ultra_duration / 58.2;
+
+  //Indoor Ultrasonic
+  digitalWrite(ULTRA_2_TRIG_PIN, LOW);
+  delay(2);
+  digitalWrite(ULTRA_2_TRIG_PIN, HIGH);
+  delay(10);
+  digitalWrite(ULTRA_2_TRIG_PIN, LOW);
+  ultra_duration = pulseIn(ULTRA_2_ECHO_PIN, HIGH);
+  ultra_distance_2 = ultra_duration / 58.2;
+
+  //LDR
+  light = analogRead(LDR_PIN);
+
+  //DHT
+  DHT.read11(DHT11_PIN);
+  temperature =(int) DHT.temperature;
+}
+
+void taskSendData(){
+  sendStr = sendLdr + String(light);
+  writeAndCheck();
+
+  sendStr = sendTemp + String(temperature);
+  writeAndCheck();
+
+  sendStr = sendUltra + String(ultra_distance_1) + "," + String(ultra_distance_2);
+  writeAndCheck();
+}
+
+void writeAndCheck()
+{
+  //Write to server
+  data = Ciao.write(CONNECTOR, SERVER_ADDR, sendStr);
+  //Check
+  Serial.print(F(" >> "));
+  Serial.println(sendStr);
+  if(!data.isEmpty()) Serial.println(F("Write Success"));
+  else Serial.println(F("Write Failed"));
+}
+
+void setup() {
+  Serial.begin(9600);
+  Ciao.begin();
+
+  pinMode(ULTRA_1_TRIG_PIN, OUTPUT);
+  pinMode(ULTRA_1_ECHO_PIN, INPUT);
+  pinMode(ULTRA_2_TRIG_PIN, OUTPUT);
+  pinMode(ULTRA_2_ECHO_PIN, INPUT);
+  pinMode(LDR_PIN, INPUT);
+}
+
+void loop() {
+  taskValue();
+  taskSendData();
+  delay(DELAY_TIME);
+}
+
---
+++ ReceiveOrder_Act_Board.ino
@@ -0,0 +1,122 @@
+#include <Wire.h>
+#include <UnoWiFiDevEd.h>
+#include <Servo.h>
+
+#define CONNECTOR         "rest"
+#define SERVER_ADDR       "158.108.165.223"
+
+#define READ_DOOR         "/data/workshop/5"
+#define READ_AIR          "/data/workshop/2"
+#define READ_LIGHT        "/data/workshop/1"
+
+#define SERVO_PIN         3
+#define LED_AIR_PIN       4
+#define LED_FRONT_PIN     5
+
+#define SERVO_DOOR_CLOSE  135
+#define SERVO_DOOR_OPEN   45
+
+#define ON                "on"
+#define OFF               "off"
+
+#define DELAY_TIME        2000
+
+CiaoData data;
+Servo door_servo;
+
+void doorOpen(){
+  door_servo.write(SERVO_DOOR_OPEN);
+}
+
+void doorClose(){
+  door_servo.write(SERVO_DOOR_CLOSE);
+}
+
+void airOn(){
+  digitalWrite(LED_AIR_PIN, HIGH);
+}
+
+void airOff(){
+  digitalWrite(LED_AIR_PIN, LOW);
+}
+
+void frontLightOn(){
+  digitalWrite(LED_FRONT_PIN, HIGH);
+}
+
+void frontLightOff(){
+  digitalWrite(LED_FRONT_PIN, LOW);
+}
+
+void door()
+{
+  data = Ciao.read(CONNECTOR, SERVER_ADDR, READ_DOOR);
+  if(!data.isEmpty()){
+    Serial.println(String(data.get(2)));
+    if(String(data.get(2)) == ON){
+      Serial.println(F("Door Open"));
+      doorOpen();
+    }
+    else{
+      Serial.println(F("Door Close"));
+      doorClose();
+    }
+  }
+  else{
+    Serial.println(F("Door Read Failed"));
+  }
+}
+
+void air()
+{
+  data = Ciao.read(CONNECTOR, SERVER_ADDR, READ_AIR);
+  if(!data.isEmpty()){
+    Serial.println(String(data.get(2)));
+    if(String(data.get(2)) == ON){
+      Serial.println(F("Air Open"));
+      airOn();
+    }
+    else{
+      Serial.println(F("Air Close"));
+      airOff();
+    }
+  }
+  else{
+    Serial.println(F("Air Read Failed"));
+  }
+}
+
+void light()
+{
+  data = Ciao.read(CONNECTOR, SERVER_ADDR, READ_LIGHT);
+  if(!data.isEmpty()){
+    Serial.println(String(data.get(2)));
+    if(String(data.get(2)) == ON){
+      Serial.println(F("Light Open"));
+      frontLightOn();
+    }
+    else{
+      Serial.println(F("Light Close"));
+      frontLightOff();
+    }
+  }
+  else{
+    Serial.println(F("Light Read Failed"));
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  Ciao.begin();
+  
+  door_servo.attach(SERVO_PIN);
+  pinMode(LED_AIR_PIN, OUTPUT);
+  pinMode(LED_FRONT_PIN, OUTPUT);
+}
+
+void loop() {
+  door();
+  air();
+  light();
+  delay(DELAY_TIME);
+}
